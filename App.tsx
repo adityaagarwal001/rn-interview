97785cf0a635ad082a5065c313527bbe08bf830d
adityaagarwal <adityaagarwal990@gmail.com>
2024-01-31 12:58:41 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {ScreenHeight} from "@freakycoder/react-native-helpers";
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ videos.ts
@@ -0,0 +1,10 @@
+export const UserContent = [
+"https://d1gnaphp93fop2.cloudfront.net/videos/multiresolution/rendition_new10.m3u8",
+"https://res.cloudinary.com/dannykeane/video/upload/sp_full_hd/q_80:qmax_90,ac_none/v1/dk-memoji-dark.m3u8",
+"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+"https://diceyk6a7voy4.cloudfront.net/e78752a1-2e83-43fa-85ae-3d508be29366/hls/fitfest-sample-1_Ott_Hls_Ts_Avc_Aac_16x9_1280x720p_30Hz_6.0Mbps_qvbr.m3u8",
+"https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
+"https://assets.afcdn.com/video49/20210722/v_645516.m3u8",
+"https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
+"https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8",
+];
